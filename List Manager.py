008ea9a6622a36d4086405be2691caab7513073b
AURANGZEB KHAN <aurangzebkha313@gmail.com>
2025-01-03 21:05:48 +0500

    hi

---
+++ List Manager.py
@@ -0,0 +1,5 @@
+my_list = []
+for i in range(5):
+    item = input(f"Enter item {i+1}: ")
+    my_list.append(item)
+print("The final list is:", my_list)
---
+++ Power-Calculator.py
@@ -0,0 +1,5 @@
+base = float(input("Enter the base: "))
+exponent = float(input("Enter the exponent: "))
+result = base ** exponent
+print(f"The result of {base} raised to the power of {exponent} is {result}")
+
