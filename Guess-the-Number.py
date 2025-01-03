487f9d074ac5454235c5872e9fcb195d32eab398
AURANGZEB KHAN <aurangzebkha313@gmail.com>
2025-01-03 21:03:07 +0500

    complete the lab task

---
+++ Guess-the-Number.py
@@ -0,0 +1,10 @@
+import random
+number_to_guess = random.randint(1, 10)
+user_guess = 0
+while user_guess != number_to_guess:
+    user_guess = int(input("Guess a number between 1 and 10: "))
+    if user_guess < number_to_guess:
+        print("Too low!")
+    elif user_guess > number_to_guess:
+        print("Too high!")
+print("Congratulations! You guessed the right number.")
