5de3cc6c97d498978ea33d2f3fddc0c5eb782b27
AURANGZEB KHAN <aurangzebkha313@gmail.com>
2025-01-03 21:28:26 +0500

    this is the new one

--- List Manager.py
+++ List Manager.py
@@ -1,5 +1,5 @@
 my_list = []
 for i in range(5):
-    item = input(f"Enter item {i+1}: ")
+    item = input("Enter item " + str(i+1) + ": ")
     my_list.append(item)
 print("The final list is:", my_list)
