782cc0292665caa720d97c7dfe7d4cfb1cb9d256
AURANGZEB KHAN <aurangzebkha313@gmail.com>
2025-01-04 14:59:01 +0500

    this is the new changing

---
+++ only for checking offline.py
@@ -0,0 +1,16 @@
+base=float(input("Enter the number for base     : "))
+exponent=float(input("Enter the number for exponet    :"))
+result=base**exponent
+print("the base which is ",base,"the exponet which is ",exponent," Result is",result)
+print("thanks for using this program")
+print("base = ",base, "exponent = " ,exponent,"result= ",result)  
+print("base",base,"exponent",exponent,"result",result)
+print("base",base ,"\n exponent",exponent,"\n result",result)
+print("base\t",base , "\t exponent", exponent , "\t result", result)
+print("base",base , "\v exponent", exponent , "\v result", result)
+print ("base\r", base , "\r exponent", exponent , "\r result", result)
+print("base\f", base , "\f exponent", exponent , "\f result", result)
+print("base\b", base , "\b exponent", exponent , "\b result", result)
+print("base\a", base , "\a exponent", exponent , "\a result", result)
+
+
