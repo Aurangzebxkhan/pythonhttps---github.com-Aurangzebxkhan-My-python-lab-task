2abc89aaff96e9cc762ed37482ef655474857842
AURANGZEB KHAN <aurangzebkha313@gmail.com>
2025-01-09 15:00:15 +0500

    this is

---
+++ list.py
@@ -0,0 +1,48 @@
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+# thilist2 = ["apple", "banana", "cherry"]
+# for item in thilist2:
+#     print(len(thilist2))
+#     break  # Exit the loop after printing the length once
+
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
+# list4 = ["abc", 34, True, 40, "male"]
+# print(list1, "\n", list2, "\n",list3,"\n", list4, "\n")
+
+# list5 = ["apple", 1, True, False, "male", 2.5, 'a', 6]
+# print(list5)
+# print(len(list5))
+
+# # Print each item in the list
+# for item in list5:
+#     print(item)
+
+# # Print the 8th item in the list (index 7)
+# print(list5[7])
+# print(list5[2:5])
+# print(list5[1:7])
+# subject=[" fundamentals","ict lab","ICT theory","discreat mathemtical","function english","islamyath"]
+# print(subject[3])
+# print(subject[1:3])
+# print(subject[-1:5])
+# print(subject[-4])
+# print(subject[-1:-5])
+# print(subject[1:-5])
+# subject = ["fundamentals", "ict lab", "ICT theory", "discreat mathemtical", "function english", "islamyath"]
+
+# # Change "ICT theory" to a new value
+# subject[2] = "Advanced ICT theory ko chang kar bas ho tayh"
+# # Print the updated list to verify the change
+# print(subject)
+# subject[2:6]="khan g","my Name","King khan","jkajdfjl","denger khan dy sir g"
+# print(subject)
+
+
+list=["apple","organ","banana","good apple ","new apple","old appple"]
+list1=[11,13,15,13,15,16]
+combinelist=list+list1
+print(combinelist)
+
