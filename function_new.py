572688b06029e7eca4f7eb3ce3d240223a4d607b
AURANGZEB KHAN <aurangzebkha313@gmail.com>
2025-01-29 22:42:57 +0500

    add some program

---
+++ function_new.py
@@ -0,0 +1,16 @@
+# print() , input(),len()
+def hello():
+    print('hello!')
+    print('hoddy!!!')
+    print('Hello there.')
+    print("this is the function when call this function in main ")
+    print("how many time do you call it .")
+    print("this is your own choice ")
+
+hello()
+
+# if __name__ == "__main__":
+#     hello()
+#     hello()
+#     hello()
+# you can call it in very where in program 
---
+++ the_none_value.py
@@ -0,0 +1,12 @@
+spam=print("Hello!")
+None==spam
+print('Hello')
+print('Wolrd')
+# when we use end='' this teh come in the same line 
+print("Hello",end='')
+print("Wolrd")
+print("Hello my frind ",end='')
+print('i am khan')
+
+print('cat','dog','mice')
+print('cat','dog','mice',sep=',')
